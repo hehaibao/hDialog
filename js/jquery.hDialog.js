c99bf4d44d439a5d03ed5dc08f7618c3b03adb50
haibao <hhb219@163.com>
2016-03-02 23:53:25 +0800

    fix issues #1
    fix issues #1

--- js/jquery.hDialog.js
+++ js/jquery.hDialog.js
@@ -2,8 +2,8 @@
 +-------------------------------------------------------------------
 * jQuery hDialog - 多功能弹出层插件
 +-------------------------------------------------------------------
-* @version 2.0.1
-* @update 2015.07.30
+* @version 2.0.2
+* @update 2016.03.02
 * @author haibao <hhb219@163.com> <http://www.hehaibao.com/>
 +-------------------------------------------------------------------
 */
@@ -12,108 +12,78 @@
 	methods = {
         init: function (options) {
            	return this.each(function() {
-				var T = $(this), _O = T.data('hDialog');
-                if(typeof(_O) == 'undefined') {
-	                	var defaults = {
-	                		title: '',              	    //弹框标题
-						box: '#HBox',               //弹框默认选择器
-						boxBg: '#ffffff',           //弹框默认背景颜色
-						modalBg: 'rgba(0,0,0,0.5)', //遮罩默认背景颜色
-						closeBg: '#cccccc',         //弹框关闭按钮默认背景颜色
-						width: 300,                 //弹框默认宽度
-						height: 270,                //弹框默认高度
-						positions: 'center',        //弹框位置(默认center：居中，top：顶部居中，left：顶部居左，bottom：底部居右)
-						effect: 'zoomOut',          //弹框关闭效果(结合animate.css里的动画，默认：zoomOut)
-						hideTime: 0,				    //弹框定时关闭(默认0:不自动关闭，以毫秒为单位)
-						resetForm: true,            //是否清空表单(默认true：清空，false：不清空)
-						modalHide: true,            //是否点击遮罩背景关闭弹框(默认true：关闭，false：不可关闭)
-						isOverlay: true,            //是否显示遮罩背景(默认true：显示，false：不显示)
-	                		closeHide: true,            //是否隐藏关闭按钮(默认true：不隐藏，false：隐藏)
-	                		escHide: true,              //是否支持ESC关闭弹框(默认true：关闭，false：不可关闭)
-	                		autoShow: false,            //是否页面加载完成后自动弹出(默认false点击弹出，true：自动弹出)
-	                		types: 1,                   //弹框类型(默认：1正常弹框，2：iframe框架)
-	                		iframeSrc: '',              //弹框类型为iframe时使用的 链接地址
-	                		iframeId: 'iframeHBox',     //弹框类型为iframe时使用的 ID
-	                		beforeShow: function(){},   //显示前的回调方法
-	                		afterHide: function(){}     //隐藏后的回调方法
-	                	};
-					_O = $.extend({}, defaults, options);
-					T.data('hDialog', _O);
-                }
-                _O = $.extend({}, _O, options);
-                
-                if(_O.autoShow != false) {
-                		methods.open(_O,T);
-                }else{
-                		T.on('click',function() { methods.open(_O,T); });
-                }
+				var T = $(this), _O = $(this).data('hDialog');
+                	var defaults = {
+		        		title: '',              	    //弹框标题
+					box: '#HBox',               //弹框默认选择器
+					boxBg: '#ffffff',           //弹框默认背景颜色
+					modalBg: 'rgba(0,0,0,0.5)', //遮罩默认背景颜色
+					width: 300,                 //弹框默认宽度
+					height: 270,                //弹框默认高度
+					positions: 'center',        //弹框位置(默认center：居中，top：顶部居中，left：顶部居左，bottom：底部居右)
+					effect: 'zoomOut',          //弹框关闭效果(结合animate.css里的动画，默认：zoomOut)
+					hideTime: 0,				    //弹框定时关闭(默认0:不自动关闭，以毫秒为单位)
+					modalHide: true,            //是否点击遮罩背景关闭弹框(默认true：关闭，false：不可关闭)
+					isOverlay: true,            //是否显示遮罩背景(默认true：显示，false：不显示)
+		        		escHide: true,              //是否支持ESC关闭弹框(默认true：关闭，false：不可关闭)
+		        		autoShow: false,            //是否页面加载完成后自动弹出(默认false点击弹出，true：自动弹出)
+		        		types: 1,                   //弹框类型(默认：1正常弹框，2：iframe框架)
+		        		iframeSrc: '',              //弹框类型为iframe时使用的 链接地址
+		        		iframeId: 'iframeHBox',     //弹框类型为iframe时使用的 ID
+		        		beforeShow: function(){},   //显示前的回调方法
+		        		afterHide: function(){}     //隐藏后的回调方法
+		        	};
+				_O = $.extend({}, defaults, options);
+				T.data('hDialog', _O);
+				_O.autoShow ? methods.open(_O,T) : T.on('click',function() { methods.open(_O,T); });
 			});
         },
         open: function (o,T) {
-        		var w,h,t,l,m,$close, headTpl = closeBtnTpl = overlayTpl = iframeTpl = '', $obj = $(o.box), title = o.title, c = T.attr("class"), modalBg = o.modalBg, closeBg = o.closeBg;
-			w = o.width != undefined ? parseInt(o.width) : 300, h = o.height != undefined ? parseInt(o.height) : 270, m = ""+parseInt(-(h/2))+'px 0 0 '+parseInt(-(w/2))+"px";
+        		var w,h,t,l,m,$close, headTpl = overlayTpl = iframeTpl = '', $o = o.autoShow ? T : $(o.box);
+			w = parseInt(o.width) || 300, h = parseInt(o.height) || 270, m = ""+parseInt(-(h/2))+'px 0 0 '+parseInt(-(w/2))+"px";
         		
-        		//重置表单
-			if(o.resetForm) {
-           	 	$obj.find('input[type=text],input[type=tel],input[type=email],input[type=date],input[type=password],textarea').val('');
-           	 	$obj.find('select option').removeAttr('selected');
-            		$obj.find('input[type=radio],input[type=checkbox]').removeAttr('checked');
-       		}
-			
 			//显示前的回调
-			methods.fire.call(this, o.beforeShow); 
+			this.fire.call(this, o.beforeShow); 
 			
 			//弹框位置
 			switch (o.positions) {
-				case 'top': 
-					t = 0; l = '50%'; m = "0 0 0 "+parseInt(-(w/2))+"px";
-				break;
-				case 'left': 
-					t = l = m = 0;
-				case 'bottom':
-					t = parseInt($W.height() - h)+'px';
-					l = parseInt($W.width() - w)+'px';
-					m = 0;
-				break;
-				default: 
-					t = l = '50%';
+				case 'top': t = 0; l = '50%'; m = "0 0 0 "+parseInt(-(w/2))+"px"; break;
+				case 'left': t = l = m = 0; break;
+				case 'bottom': t = parseInt($W.height() - h)+'px'; l = parseInt($W.width() - w)+'px'; m = 0; break;
+				default: t = l = '50%';
 			}
-			
-			//关闭按钮
-			if(o.closeHide != false) closeBtnTpl = '<a id="HCloseBtn" title="关闭" style="width:24px;height:24px;line-height:24px;display:inline-block;cursor:pointer;background-color:'+closeBg+';color:#fff;font-size:1.4em;text-align:center;position:absolute;top:8px;right:8px;"><span style="width:24px;height:24px;display:inline-block;">×</span></a>';
 
-			//弹框标题
-			if(o.title != '') headTpl = '<div id="HTitle" style="padding:10px 45px 10px 12px;border-bottom:1px solid #ddd;background-color:#f2f2f2;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;">'+o.title+'</div>';
+			//弹框标题和关闭按钮
+			headTpl = (o.title != '' && $('#HTitle').length <= 0) ? '<div id="HTitle">'+o.title+'</div> <a id="HCloseBtn" title="关闭"><span>&times;</span></a>' : '';
 			
 			//遮罩背景层
-			if(o.isOverlay != false) overlayTpl = "<div id='HOverlay' style='width:"+$D.width()+"px;height:"+$D.height()+"px;background-color:"+modalBg+";position:fixed;top:0;left:0;z-index:9999;'></div>";
+			overlayTpl = (o.isOverlay && $('#HOverlay').length <= 0) ? "<div id='HOverlay' style='width:"+$D.width()+"px;height:"+$D.height()+"px;background-color:"+o.modalBg+";position:fixed;top:0;left:0;z-index:9999;'></div>" : '';
        	 	
-			//显示弹框
-			if(o.types == 2){ iframeTpl = '<iframe width="' + o.width + '" height="' + o.height + '" frameborder="0" scrolling="auto" src="' + o.iframeSrc + '"></iframe>'; $B.append('<div id="'+o.iframeId+'"></div>'); $obj = $('#'+o.iframeId); }
-        		if(o.autoShow != false) $obj = T;
-        		$B.stop().append(overlayTpl).find($obj).css({'background-color':'rgba(255,255,255,1)','position':'fixed','top':t,'left':l,'z-index':999999,'margin':m,'width':o.width,'height':o.height}).removeAttr('class').addClass('animated '+c).prepend(headTpl+closeBtnTpl+iframeTpl).show();
+			//iframe框架
+			iframeTpl = o.types == 2 ? '<iframe id="'+o.iframeId+'" width="'+o.width+'" height="'+o.height+'" frameborder="0" scrolling="auto" src="'+o.iframeSrc+'"></iframe>' : '';
+			
+        		//显示弹框
+        		$B.stop().append(overlayTpl).find($o).css({'background-color':o.boxBg,'position':'fixed','top':t,'left':l,'z-index':999999,'margin':m,'width':o.width,'height':o.height}).removeAttr('class').addClass('animated '+T.attr("class")).prepend(headTpl+iframeTpl).show();
         
         		//默认关闭
-        		$close = $('#HCloseBtn');
-        		if(o.modalHide) $close = $('#HOverlay,#HCloseBtn');
+        		$close = o.modalHide ? $('#HOverlay,#HCloseBtn') : $('#HCloseBtn');
 			$close.on('click',function(){ methods.close(o,T); });
 			
 			//定时关闭
-        		if(o.hideTime != 0) setTimeout(function(){ methods.close(o,T); }, parseInt(o.hideTime));
+        		o.hideTime != 0 && setTimeout(function(){ methods.close(o,T); }, parseInt(o.hideTime));
         		
 			//支持ESC关闭
-			if(o.escHide) $D.keyup(function(){ if(event.keyCode === 27) methods.close(o,T); });
+			o.escHide && $D.on('keyup',function(){ if(event.keyCode === 27) methods.close(o,T); });
         },
 	    close: function (o,T) {
-	    		var $obj = (o.autoShow != false) ? T : $(o.box);
-	    		methods.remove('#HOverlay,#HCloseBtn,#HTitle,#'+o.iframeId);
-	    		$obj.removeAttr('class').addClass('animated '+o.effect);
-			if($obj.hasClass(o.effect)){ setTimeout(function(){ $obj.removeAttr('style').hide(); },300); }
-			this.fire.call(this, o.afterHide); //隐藏后的回调
+	    		var $o = o.autoShow ? T : $(o.box);
+	    		$o.removeAttr('class').addClass('animated '+o.effect);
+			$o.hasClass(o.effect) && setTimeout(function(){ $o.removeAttr('style').hide(); },300);
+			this.remove('#HOverlay,#HTitle,#'+o.iframeId);
+			methods.fire.call(this, o.afterHide);
 	    },
         remove: function (a) { $(a).remove(); },
-	    fire: function (event, data) { if($.isFunction(event)) { return event.call(this, data); } },
-	    destroy: function() { return $(this).each(function() { var $this = $(this); $this.removeData('hDialog'); }); }
+	    fire: function (event, data) { if($.isFunction(event)) { return event.call(this, data); } }
     };
     
 	$.fn.hDialog = function (method) {
@@ -153,14 +123,13 @@
 		 */
         tooltip: function (t1, t2, t3, callback) {
         		t1 = t1 != undefined ? t1 : 'Error...'; 	t2 = t2 != undefined ? parseInt(t2) : 2500;
-        		var tip = '<div class="HTooltip shake animated" style="width:280px;padding:10px;text-align:center;background-color:#D84C31;color:#fff;position:fixed;top:10px;left:50%;z-index:100001;margin-left:-150px;box-shadow:1px 1px 5px #333;-webkit-box-shadow:1px 1px 5px #333;">'+t1+'</div>';
-	    		if(t3 == true && t3 != undefined) { tip = '<div class="HTooltip bounceInDown animated" style="width:280px;padding:10px;text-align:center;background-color:#5cb85c;color:#fff;position:fixed;top:10px;left:50%;z-index:100001;margin-left:-150px;box-shadow:1px 1px 5px #333;-webkit-box-shadow:1px 1px 5px #333;">'+t1+'</div>'; }
+	    		var tip = (t3 && t3 != undefined) ? '<div class="HTooltip bounceInDown animated" style="width:280px;padding:10px;text-align:center;background-color:#5cb85c;color:#fff;position:fixed;top:10px;left:50%;z-index:100001;margin-left:-150px;box-shadow:1px 1px 5px #333;-webkit-box-shadow:1px 1px 5px #333;">'+t1+'</div>' : '<div class="HTooltip shake animated" style="width:280px;padding:10px;text-align:center;background-color:#D84C31;color:#fff;position:fixed;top:10px;left:50%;z-index:100001;margin-left:-150px;box-shadow:1px 1px 5px #333;-webkit-box-shadow:1px 1px 5px #333;">'+t1+'</div>';
 	    		methods.remove('.HTooltip');
 	    		$B.stop().append(tip);
 	    		clearTimeout(t);
 	    		var t = setTimeout(function() { 
 	    			methods.remove('.HTooltip'); 
-	    			if(callback != undefined){ methods.fire.call(this, callback); } //隐藏后的回调方法 
+	    			callback != undefined && methods.fire.call(this, callback); //隐藏后的回调方法 
 	    		},t2);
         },
         /**
@@ -174,14 +143,10 @@
 	        	$B.stop().append('<a id="HGoTop" href="javascript:;" style="width:40px;height:40px;line-height:40px;border-radius:50%;display:inline-block;text-align:center;background:#333;color:#fff;position:fixed;bottom:'+b+';right:'+r+';z-index:100000;">Top</a>').find('#HGoTop').hide();
 	        	$T = $('#HGoTop');
 	        	$W.on('scroll',function(){
-	        		if($W.scrollTop() > 150){
-					$T.removeAttr('class').addClass('animated rollIn').show();
-	        		}else{
-					$T.removeAttr('class').addClass('animated rollOut');
-				}
+	        		$W.scrollTop() > 150 ? $T.removeAttr('class').addClass('animated rollIn').show() : $T.removeAttr('class').addClass('animated rollOut');
 	        	});
 	        	$T.on('click',function(){ 
-	        		$('body,html').animate({ scrollTop:0 },500); return false; //返回顶部按钮点击事件
+	        		$('body,html').animate({ scrollTop:0 },500); return; //返回顶部按钮点击事件
 	        	});
         },
         /**
@@ -200,12 +165,12 @@
     			contentTpl = '<div id="hDialog-content" style="padding:25px 15px;text-align:center;">' + content + '</div>',
     			maskTpl = '<div id="hDialog-mask" style="width:100%;height:100%;background-color:rgba(0,0,0,0.6);position:fixed;top:0;left:0;z-index:99999;"></div>';  		
     			if(type === 'confirm'){ footerTpl = '<div id="hDialog-footer" style="padding:10px;border-top:1px solid #ddd;text-align:right;">'+ okTpl + cancelTpl +'</div>'; }
-    			if(isLock != true) tpl += maskTpl;
+    			if(!isLock) tpl += maskTpl;
     			tpl += '<div id="hDialog-wrap" class="animated fadeIn" style="width:'+width+'px;border-radius:5px;box-shadow:4px 4px 10px #666;-webkit-box-shadow:4px 4px 10px #666;background-color:#ffffff;border:1px solid #eee;position:fixed;top:35%;left:50%;margin:'+margin+';z-index:100000;">' + headerTpl + contentTpl + footerTpl + '</div>'; 
     			methods.remove('#hDialog-wrap,#hDialog-mask');
     			$B.stop().append(tpl);
     			if(time !== 0) { clearTimeout(t); t = setTimeout(function() { $.closeDialog(callback); },time); }
-    			$('a:contains('+okText+')').on("click", function() { if(callback != undefined){ methods.fire.call(this, callback); } });
+    			$('a:contains('+okText+')').on("click", function() { callback != undefined && methods.fire.call(this, callback); });
             $('a:contains('+cancelText+')').on("click", function() { $.closeDialog(); });
         },
         /**
@@ -214,7 +179,7 @@
 		 */
         closeDialog: function (callback) {
         		methods.remove('#hDialog-wrap,#hDialog-mask'); 
-        		if(callback != undefined) methods.fire.call(this, callback);
+        		callback != undefined && methods.fire.call(this, callback);
         }
 	});
 })(jQuery, window, document);
